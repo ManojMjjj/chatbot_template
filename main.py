60047f9d26d44d7e0ba06ada3fe40c4b91a3a121
ManojMjjj <85756883+ManojMjjj@users.noreply.github.com>
2022-02-03 17:20:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,4 +15,4 @@ def hello():
     return "Successfully installed all the Libraries and is running. Upload the files now"
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=8080,debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
